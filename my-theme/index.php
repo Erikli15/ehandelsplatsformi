192b1c6748e66895e7bb1fead8610e22a6c78569
Erikli15 <erik.lindner@workmail.com>
2024-09-02 11:52:52 +0200

    my-theme

---
+++ my-theme/index.php
@@ -0,0 +1,27 @@
+<?php
+get_header();
+
+if (is_tax() || is_category()) {
+	// Get the current term
+	$term = get_queried_object();
+
+	// Output the term name
+	echo '<h1>' . esc_html($term->name) . '</h1>';
+}
+
+?>
+
+<?php
+if (have_posts()):
+
+	while (have_posts()):
+		the_post();
+		the_title();
+		the_content();
+	endwhile;
+
+endif;
+?>
+
+<?php
+get_footer();
---
+++ my-theme/page.php
@@ -0,0 +1,41 @@
+<?php
+get_header(); ?>
+This is a page:
+<?php
+if (have_posts()):
+
+	while (have_posts()):
+		the_post();
+
+		$query = new WP_Query(
+			array(
+				'post_parent' => get_the_ID(),
+				'post_type' => 'page',
+				'orderby' => 'menu_order',
+				'order' => 'ASC'
+			)
+		);
+
+		while ($query->have_posts()) {
+			$query->the_post();
+
+			?><a href="<?php echo (get_permalink(get_the_ID())); ?>">
+				<?php
+				the_title();
+				?>
+			</a>
+			<?php
+		}
+
+		wp_reset_postdata();
+
+
+		the_title();
+		the_content();
+
+	endwhile;
+
+endif;
+?>
+<?php
+get_footer();
