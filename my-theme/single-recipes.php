f34783ce2cc51eb5fa558b30f313283951541603
Erikli15 <erik.lindner@workmail.com>
2024-09-06 14:10:00 +0200

    recept casten posetype

--- my-theme/single-recipes.php
+++ my-theme/single-recipes.php
@@ -5,6 +5,18 @@ Template Name:  recipes page
 ?>
 
 <form method="POST">
-    <textarea name="" id=""></textarea>
-    <input type="button" value="Spara">
+    <textarea name="recipe_content" id="recipe_content"></textarea>
+    <input type="submit" value="Spara" name="submit_recipe">
 </form>
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['recipe_content'])) {
+        add_post_meta(get_the_id(), 'recipe_message', $_POST['recipe_content']);
+    }
+}
+$recipes = get_post_meta(get_the_id(), 'recipe_message', false);
+foreach ($recipes as $recipe) {
+    echo ('<div>' . $recipe . '</div>');
+}
