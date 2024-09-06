bda0571956d795ba9c402cb8dc2fa41fec1097f9
Erikli15 <erik.lindner@workmail.com>
2024-09-06 14:09:30 +0200

    serch produkt

--- my-theme/single-collegtions.php
+++ my-theme/single-collegtions.php
@@ -4,15 +4,44 @@ Template Name:  collegtions page
 */
 ?>
 
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST[""])) {
 
+<form method="POST">
+    <input type="text" name="search_query" id="search_query">
+    <input type="submit" value="Sök" name="submit_search">
+</form>
+
+
+
+
+
+<?php
+
+if (isset($_POST['submit_search'])) {
+
+    $search_query = sanitize_text_field($_POST['search_query']);
+    $args = array(
+        'post_type' => 'product',
+        'posts_per_page' => -1, // Hämta alla produkter
+        's' => $search_query // Search for products based on the search query
+    );
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $product = wc_get_product(get_the_ID());
+            // Gör något med produkten, t.ex. visa dess namn
+            echo '<div>';
+            echo $product->get_name();
+            // Add a link or button to add the product to the collection list
+            echo ' <a href="#" data-product-id="' . $product->get_id() . '">Lägg till i samling</a>';
+            echo '</div>';
+
+        }
+
+        wp_reset_postdata();
+    } else {
+        echo 'Inga produkter hittades.';
     }
 }
 ?>
-
-<form method="POST">
-    <input type="text">
-    <input type="button" value="Spara">
-</form>
